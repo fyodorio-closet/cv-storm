07b18508a9ddca8e95a2405efed5c737b17422b4
loenko <loenko.js@gmail.com>
2019-01-24 05:28:08 +0500

    initial commit

---
+++ main.js
@@ -0,0 +1,7 @@
+(function () {
+	// const app = document.getElementById("app");
+	// const header = document.createElement("H1");
+	// const testText = document.createTextNode("TEST");
+	// header.appendChild(testText);
+	// app.appendChild(header);
+})();
